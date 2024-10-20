6ed478ba4b6ad88f9b2386fb899535585139a24f
Vitor Serra <vitorserracardias2@gmail.com>
2024-10-19 23:27:50 -0300

    Questão 3

---
+++ 3-codigo.py
@@ -0,0 +1,12 @@
+# 3) Observe o trecho de código abaixo:
+# int INDICE = 12, SOMA = 0, K = 1;
+# enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+# imprimir(SOMA);
+
+# Ao final do processamento, qual será o valor da variável SOMA?
+
+indice, soma, k = 12, 0, 1
+while k < indice:
+    k += 1
+    soma += k
+print(soma)
